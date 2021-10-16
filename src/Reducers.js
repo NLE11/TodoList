873065ccb0c4e34e00e9d278d72bff87295bc656
Louis_VT <louis9286@icloud.com>
2021-10-16 02:42:45 -0500

    Add reducers

---
+++ src/Reducers.js
@@ -0,0 +1,53 @@
+import nextId from "react-id-generator"; // npm install react-id-generator
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+    case "REGISTER":
+      return action.username;
+    case "LOGOUT":
+      return "";
+    default:
+      return state;
+  }
+}
+
+function taskReducer(state, action) {
+  switch (action.type) {
+    case "CREATE_TASK":
+      const newTask = {
+        index: nextId(),
+        title: action.title,
+        description: action.description,
+        dateCreated: action.dateCreated,
+        dateCompleted: action.dateCompleted,
+        complete: action.complete,
+      };
+      return [newTask, ...state];
+    case "TOGGLE_TASK":
+      const remainTask = state.filter((task) => task.index !== action.index);
+      const updateTask = {
+        index: action.index,
+        title: action.title,
+        description: action.description,
+        dateCreated: action.dateCreated,
+        dateCompleted: action.dateCompleted,
+        complete: action.complete,
+      };
+      return [...remainTask, updateTask]; // prevTask has some values not just one
+    case "DELETE_TASK":
+      const prevTask = state.filter((task) => task.index !== action.index);
+      return [...prevTask]; // only return the prev task
+    default:
+      return state;
+  }
+}
+
+export default function appReducer(state, action) {
+  //In this appReducer function, we are going to call the other two reducer functions, and return the full state tree:
+
+  return {
+    user: userReducer(state.user, action),
+    tasks: taskReducer(state.tasks, action),
+  };
+}
